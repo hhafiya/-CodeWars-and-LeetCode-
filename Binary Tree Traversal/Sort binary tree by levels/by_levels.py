1869b29f072660efa987edda1d6d3e6f8c024538
Your Name <you@example.com>
2024-04-21 14:03:40 +0300

    tree by levels tash and all test passed

---
+++ Binary Tree Traversal/Sort binary tree by levels/by_levels.py
@@ -0,0 +1,20 @@
+from collections import deque
+class Node:
+    def __init__(self, L, R, n):
+        self.left = L
+        self.right = R
+        self.value = n
+
+def tree_by_levels(node):
+    if not node:
+        return []
+    result = []
+    queue = deque([node])
+    while queue:
+        node = queue.popleft()
+        result.append(node.value)
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+    return result
